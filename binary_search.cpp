b72d729d46ae915e1d85eff66f0b97e0bff4d4e1
Preso <119664913+presotech@users.noreply.github.com>
2024-01-07 19:08:47 -0700

    Create binary_search.cpp

---
+++ binary_search.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int randint(int min, int max);
+
+int main()
+{
+    srand(time(NULL));
+
+    int guess;
+    int limit = 10;
+    int secret = randint(1,100);
+
+    bool win = false;
+
+    for (int i = 1; i <= limit; i++)
+    {
+        cout << "Guess: ";
+        cin >> guess;
+
+        if (guess > secret)
+        {
+            cout << "Too high!" << endl;
+        }
+
+        else if (guess < secret)
+        {
+            cout << "Too low!" << endl;
+        }
+
+        else
+        {
+            win = true;
+            cout << "You win! Guesses used: " << i << endl;
+            break;
+        }
+    }
+
+    if (!win)
+    {
+        cout << "You lose!" << endl;
+    }
+}
+
+int randint(int min, int max)
+{
+    return min + rand() % (max - min + 1);
+}
